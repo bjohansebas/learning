11dfc0e69227ff09fd9b77e02fd9b2d9ec9d7828
Sebastian Beltran <bjohansebas@gmail.com>
2024-06-13 19:02:28 -0500

    improve tic tac toe

---
+++ midudev/react-2024/projects/02-tic-tac-toe/src/components/Square.jsx
@@ -0,0 +1,13 @@
+export const Square = ({ children, isSelected, updateBoard, index }) => {
+	const className = `square ${isSelected ? "is-selected" : ""}`;
+
+	const handleClick = () => {
+		updateBoard(index);
+	};
+	return (
+		// biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
+		<div onClick={handleClick} className={className}>
+			{children}
+		</div>
+	);
+};
