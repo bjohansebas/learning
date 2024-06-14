1ca22e94e9e2cda45e3f1acc348a926e3691b436
Sebastian Beltran <bjohansebas@gmail.com>
2024-06-14 18:08:49 -0500

    add follow mouse component

--- midudev/react-2024/projects/03-mouse-follower/src/App.jsx
+++ midudev/react-2024/projects/03-mouse-follower/src/App.jsx
@@ -1,10 +1,46 @@
-import { useState } from "react";
-import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [enabled, setEnabled] = useState(false);
+	const [position, setPosition] = useState({ x: 0, y: 0 });
+	useEffect(() => {
+		const handleMove = (event) => {
+			const { clientX, clientY } = event;
 
-	return <h3>Proyecto 3</h3>;
+			setPosition({ x: clientX, y: clientY });
+		};
+
+		if (enabled) {
+			window.addEventListener("pointermove", handleMove);
+		}
+
+		return () => {
+			window.removeEventListener("pointermove", handleMove);
+		};
+	}, [enabled]);
+
+	return (
+		<main>
+			<div
+				style={{
+					position: "absolute",
+					backgroundColor: "rgba(0,0,0,0.5)",
+					borderRadius: "50%",
+					border: "1px solid #fff",
+					opacity: 0.8,
+					pointerEvents: "none",
+					left: -20,
+					top: -20,
+					width: 40,
+					height: 40,
+					transform: `translate(${position.x}px,${position.y}px)`,
+				}}
+			/>
+			<button onClick={() => setEnabled(!enabled)} type="button">
+				{enabled ? "Desactivar" : "Activar"} seguir puntero
+			</button>
+		</main>
+	);
 }
 
 export default App;
