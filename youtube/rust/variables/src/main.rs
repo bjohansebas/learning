b3a4c789efac6bd3f8ef400f53fef16d1be11bf8
Sebastian Beltran <bjohansebas@gmail.com>
2024-06-25 13:52:07 -0500

    variable class

---
+++ youtube/rust/variables/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut x = 3;
+
+    println!("El valor de x es: {}",x);
+
+    x = 7;
+    println!("x is {}", x);
+
+    const PI: f32 = 3.1416;
+    println!("PI is {}",PI);
+
+    let x = "l";
+    println!("x is {}",x)
+}
