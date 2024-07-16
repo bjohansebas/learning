32d3ef4d121ae6d13dabc14a973f23ac63b6aac9
Sebastian Beltran <bjohansebas@gmail.com>
2024-07-15 20:31:29 -0500

    add new class

---
+++ programadorATS/05-exercise/main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int age;
+    char sex[10];
+    float height;
+
+    cout<<"Insert your age: ";cin>>age;
+    cout<<"Insert your gender: ";cin>>sex;
+    cout<<"Insert your height in meter: ";cin>>height;
+
+    cout<<"\nEdad: "<<age;
+    cout<<"\nSex: "<<sex;
+    cout<<"\nHeight in meter: "<<height;
+
+
+    return 0;
+}
