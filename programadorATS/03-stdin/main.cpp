75981fec11303bfd7d29e0f73ec476ac9bd65f36
Sebastian Beltran <bjohansebas@gmail.com>
2024-07-15 20:10:14 -0500

    add stdin of user

---
+++ programadorATS/03-stdin/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int number;
+
+    cout<<"Insert a number: ";
+
+    cin>>number;
+
+    cout<<"\nThe insert number was: "<< number;
+    
+    return 0;
+}
