ade4e3caff73f18854e3040b831e2febc4b9663b
Sebastian Beltran <bjohansebas@gmail.com>
2024-07-15 19:58:31 -0500

    add type of basic data

---
+++ programadorATS/02-type-of-data/main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    // Number
+    int number = 15;
+    cout<<number<<endl;
+
+    // Float - less
+    float decimal_l = 15.85;
+    cout<<decimal_l<<endl;
+
+    // Decimal - more
+    double decimal_m = 16.4576;
+    cout<<decimal_m<<endl;
+
+    // Character
+    char word = 'a';
+    cout<<word<<endl;
+
+    return 0;
+}
