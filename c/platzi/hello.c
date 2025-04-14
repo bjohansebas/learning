11294e371b6f1c80c6ab7afd048f4b557befb876
Sebastian Beltran <bjohansebas@gmail.com>
2025-04-13 19:07:19 -0500

    push

---
+++ c/platzi/hello.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    // This is a simple C program that prints "Hello, World!" to the console.
+    // It demonstrates the basic structure of a C program.
+    printf("Hello, World!\n");
+    return 0;
+}
+
+// Integers:
+// int 4 bytes -2,147,483,648 to 2,147,483,647
+// unsigned int 4 bytes 0 to 4,294,967,295
+// short 2 bytes -32,768 to 32,767
+// unsigned short 2 bytes 0 to 65,535
+// long 8 bytes -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+// unsigned long 8 bytes 0 to 18,446,744,073,709,551,615
+
+// Floating-point numbers:
+// float 4 bytes 1.2E-38 to 3.4E+38 6 decimal places
+// double 8 bytes 2.3E-308 to 1.7E+308 15 decimal places
+// long double 10 bytes 3.4E-4932 to 1.1E+4932 19 decimal places
+
+// void: No value 
+// 1. Function return void type
+//   void function()
+// 2. Function parameter void type
+//   int random(void)
+// 3. Pointer to void typ
